c1539d84a98818850ad40820ba5d41a5b8277ce9
StardustIsArt <yocius_sa@icloud.com>
2022-06-14 18:05:30 -0500

    rendered word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-  function search(event) {
-    event.preventDefault();
-  }
+  let [results, setResults] = useState(null);
+ 
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
+  }
+   
+  function search(event) {
+    event.preventDefault();
   }
 
   function handleKeywordChange(event) {
@@ -28,6 +33,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
